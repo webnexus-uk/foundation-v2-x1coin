a8b814542be368fd111fcd6c3cb63e2edaf135c8
Coding Nexus <info@webnexus.co.uk>
2024-04-12 09:15:10 +0100

    Update

--- exports.js
+++
@@ -1,8 +0,0 @@
-const Pool = require('./stratum/main/pool');
-
-////////////////////////////////////////////////////////////////////////////////
-
-exports.algorithms = require('./stratum/main/algorithms');
-exports.builder = function(config, configMain, callback) {
-  return new Pool(config, configMain, callback);
-};
